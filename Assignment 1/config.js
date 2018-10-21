71e98df830317c37da7ece957ec11dd43433fb15
ZhekaVasil <zheka_1408@mail.ru>
2018-10-21 20:44:40 +0300

    Intendts

--- Assignment 1/config.js
+++ Assignment 1/config.js
@@ -2,15 +2,16 @@ const environments = {};
 
 
 environments.staging = {
-    httpPort: 3000,
-    httpsPort: 3001,
-    envName: 'staging'
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'staging'
+  
 };
 
 environments.production = {
-    httpPort: 5000,
-    httpsPort: 5001,
-    envName: 'production'
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production'
 };
 
 const currentEnvironment = typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
--- Assignment 1/index.js
+++ Assignment 1/index.js
@@ -4,56 +4,56 @@ const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 
 const httpServer = http.createServer((req, res) => {
-    unifiedServer(req, res);
+  unifiedServer(req, res);
 });
 
 httpServer.listen(config.httpPort, () => {
-    console.log(`The http server is listening on port ${config.httpPort}`);
+  console.log(`The http server is listening on port ${config.httpPort}`);
 });
 
 const unifiedServer = (req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  
+  const decoder = new StringDecoder('utf-8');
+  
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
     
-    const decoder = new StringDecoder('utf-8');
+    const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    const data = {
+      payload: buffer
+    };
     
-    let buffer = '';
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+      payload = typeof payload === 'object' ? payload : {};
+      
+      const payloadString = JSON.stringify(payload);
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
     });
-    req.on('end', () => {
-        buffer += decoder.end();
-        
-        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        const data = {
-            payload: buffer
-        };
-        
-        chosenHandler(data, (statusCode, payload) => {
-            statusCode = typeof statusCode === 'number' ? statusCode : 200;
-            payload = typeof payload === 'object' ? payload : {};
-            
-            const payloadString = JSON.stringify(payload);
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-        });
-    });
+  });
 };
 
 const handlers = {};
 
 handlers.hello = (data, callback) => {
-    const message = {
-        message: data.payload ? `Your message was ${data.payload}` : 'There is no message'
-    };
-    callback(200, message)
+  const message = {
+    message: data.payload ? `Your message was ${data.payload}` : 'There is no message'
+  };
+  callback(200, message)
 };
 
 handlers.notFound = (data, callback) => {
-    callback(404)
+  callback(404)
 };
 const router = {
-    'hello': handlers.hello
+  'hello': handlers.hello
 };
